2cf631df1a1fca858ef78d59555ad9c56dfb1cf8
Nicholas Valbusa <squallstar@gmail.com>
2016-09-08 09:26:12 +0200

    rows template

--- js/interface.js
+++ js/interface.js
@@ -35,6 +35,24 @@ $('#app')
     $contents.html('');
     $contents.append('<a href="#" data-back>Back to data sources</a>');
     $contents.append('<h1>' + name + '</h1>');
+    $contents.append('<form><input type="file" /></form><hr />');
+
+    Fliplet.DataSources.connect(id).then(function (source) {
+      return source.find();
+    }).then(function (rows) {
+      var tableTpl = '';
+      rows.forEach(function (row) {
+        var tpl = '';
+        Object.keys(row).forEach(function (key) {
+          tpl += '<td>' + row[key] + '</td>';
+        });
+        tableTpl += '<tr>' + tpl + '</tr>';
+      });
+
+      tableTpl = '<table><tbody>' + tableTpl + '</tbody></table>';
+
+      $contents.append(tableTpl);
+    });
   })
   .on('click', '[data-delete-source]', function (event) {
     event.preventDefault();
