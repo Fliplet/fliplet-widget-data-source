6ebf3d756802dc31a4273a20bac40d5c1d3d1584
Nicholas Valbusa <squallstar@gmail.com>
2016-09-08 14:37:54 +0200

    refactor and import

--- js/interface.js
+++ js/interface.js
@@ -3,6 +3,9 @@ var templates = {
   dataSource: template('dataSource')
 };
 
+var currentDataSource;
+var currentDataSourceId;
+
 // Function to compile a Handlebars template
 function template(name) {
   return Handlebars.compile($('#template-' + name).html());
@@ -13,6 +16,31 @@ function getDataSources(folderId) {
   $contents.html('<button data-create-source class="btn btn-primary">Create new </button><hr />');
   Fliplet.DataSources.get().then(function (response) {
     response.dataSources.forEach(renderDataSource);
+  });
+}
+
+function fetchCurrentDataSourceEntries() {
+  Fliplet.DataSources.connect(currentDataSourceId).then(function (source) {
+    currentDataSource = source;
+    return source.find();
+  }).then(function (rows) {
+    if (!rows.length) {
+      return;
+    }
+
+    var tableHead = '<thead>' + Object.keys(rows[0]).map(function (column) {
+      return '<th>' + column + '</th>';
+    }).join('') + '</thead>';
+
+    var tableBody = '<tbody>' + rows.map(function (row) {
+      return '<tr>' + Object.keys(row).map(function (key) {
+        return '<td>' + row[key] + '</td>';
+      }).join('') + '</tr>';
+    }).join('') + '</tbody>';
+
+    tableTpl = '<table class="table">' + tableHead + tableBody + '</table>';
+
+    $contents.find('#entries').html(tableTpl);
   });
 }
 
@@ -29,39 +57,15 @@ $('#app')
   })
   .on('click', '[data-browse-source]', function (event) {
     event.preventDefault();
-    var id = $(this).closest('li').data('id');
+    currentDataSourceId = $(this).closest('li').data('id');
     var name = $(this).html();
 
     $contents.html('');
     $contents.append('<a href="#" data-back>Back to data sources</a>');
     $contents.append('<h1>' + name + '</h1>');
-    $contents.append('<form><input type="file" /></form><hr />');
+    $contents.append('<form>Import data: <input type="file" /></form><hr /><div id="entries"></div>');
 
-    Fliplet.DataSources.connect(id).then(function (source) {
-      return source.find();
-    }).then(function (rows) {
-      var tableTpl = '';
-
-      if (!rows.length) {
-        return;
-      }
-
-      var tableHead = '<thead>' + Object.keys(rows[0]).map(function (column) {
-        return '<th>' + column + '</th>';
-      }) + '</thead>';
-
-      rows.forEach(function (row) {
-        var tpl = '';
-        Object.keys(row).forEach(function (key) {
-          tpl += '<td>' + row[key] + '</td>';
-        });
-        tableTpl += '<tr>' + tpl + '</tr>';
-      });
-
-      tableTpl = '<table class="table">' + tableHead + '<tbody>' + tableTpl + '</tbody></table>';
-
-      $contents.append(tableTpl);
-    });
+    fetchCurrentDataSourceEntries();
   })
   .on('click', '[data-delete-source]', function (event) {
     event.preventDefault();
@@ -82,6 +86,18 @@ $('#app')
     Fliplet.DataSources.create({
       name: sourceName
     }).then(renderDataSource);
+  })
+  .on('change', 'input[type="file"]', function (event) {
+    var $input = $(this);
+    var file = $input[0].files[0];
+    var formData = new FormData();
+
+    formData.append('file', file);
+
+    currentDataSource.import(formData).then(function (files) {
+      $input.val('');
+      fetchCurrentDataSourceEntries();
+    });
   });
 
 // Fetch data sources when the provider starts
