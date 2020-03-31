82af6a991ca972f86622830ace3cbdade2f58e83
Nicholas Valbusa <squallstar@gmail.com>
2020-03-31 19:36:51 +0200

    css fixes

--- js/interface.templates.js
+++ js/interface.templates.js
@@ -99,7 +99,7 @@ this["Fliplet"]["Widget"]["Templates"]["templates.versions"] = Handlebars.templa
 },"2":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "            <div class=\"btn-group\" role=\"group\">\n              <button type=\"button\" class=\"btn btn-default dropdown-toggle\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">\n                Actions\n                <span class=\"caret\"></span>\n              </button>\n              <ul class=\"dropdown-menu\">\n                <li><a href=\"#\" data-version-preview=\""
+  return "            <div class=\"btn-group group-relative\" role=\"group\">\n              <button type=\"button\" class=\"btn btn-default dropdown-toggle\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">\n                Actions\n                <span class=\"caret\"></span>\n              </button>\n              <ul class=\"dropdown-menu menu-right\">\n                <li><a href=\"#\" data-version-preview=\""
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
     + "\">Preview data</a></li>\n                <li><a href=\"#\" data-version-restore=\""
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
@@ -109,7 +109,7 @@ this["Fliplet"]["Widget"]["Templates"]["templates.versions"] = Handlebars.templa
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<table class=\"table\">\n  <thead>\n    <th>Version</th>\n    <th>Modified by</th>\n    <th>Entries</th>\n    <th>Columns</th>\n    <th style=\"width:230px\">Actions</th>\n  </thead>\n  <tbody>\n    <tr class=\"success\">\n      <td><span class=\"label label-success\">Current</span> modified "
+  return "<table class=\"table\">\n  <thead>\n    <th>Version</th>\n    <th>Modified by</th>\n    <th>Entries</th>\n    <th>Columns</th>\n    <th>Actions</th>\n  </thead>\n  <tbody>\n    <tr class=\"success\">\n      <td><span class=\"label label-success\">Current</span> modified "
     + alias4(((helper = (helper = helpers.updatedAt || (depth0 != null ? depth0.updatedAt : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"updatedAt","hash":{},"data":data}) : helper)))
     + "</td>\n      <td>"
     + alias4(((helper = (helper = helpers.action || (depth0 != null ? depth0.action : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"action","hash":{},"data":data}) : helper)))
