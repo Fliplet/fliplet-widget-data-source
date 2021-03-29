00eefacf892b3745090ab39475c64356daa62cf2
ivandevupp <74735207+ivandevupp@users.noreply.github.com>
2021-03-29 14:41:20 +0300

    Remove empty columns that have the Column (X) format across the whole data source entries

--- js/interface.js
+++ js/interface.js
@@ -57,6 +57,8 @@ var definitionEditor = CodeMirror.fromTextArea($('#definition')[0], {
   mode: 'javascript'
 });
 
+var emptyColumnNameRegex = /^Column\s\([0-9]+\)$/;
+
 // Fetch all data sources
 function getDataSources() {
   $initialSpinnerLoading.addClass('animated');
@@ -390,6 +392,44 @@ function trimColumns(columns) {
   });
 }
 
+function getEmptyColumns(columns, entries) {
+  var emptyColumns = _.filter(columns, function(column) {
+    return emptyColumnNameRegex.test(column);
+  });
+
+  if (!emptyColumns.length) {
+    return [];
+  }
+
+  _.forEach(entries, function(entry) {
+    // Stop iteration through entries if all empty columns have values (removed from array)
+    if (!emptyColumns.length) {
+      return false;
+    }
+
+    for (var i = emptyColumns.length - 1; i >= 0; i--) {
+      if (entry.data[emptyColumns[i]] !== null && entry.data[emptyColumns[i]] !== undefined && entry.data[emptyColumns[i]] !== '') {
+        var notEmptyColumnIndex = emptyColumns.indexOf(emptyColumns[i]);
+  
+        if (notEmptyColumnIndex !== -1) {
+          emptyColumns.splice(notEmptyColumnIndex, 1);
+        }
+      }
+    }
+  });
+
+  return emptyColumns;
+}
+
+function removeEmptyColumnsInEntries(entries, emptyColumns) {
+  return entries.map(function(entry) {
+    entry.data = _.omitBy(entry.data, function(value, key) {
+      return emptyColumns.includes(key);
+    });
+    return entry;
+  });
+}
+
 function saveCurrentData() {
   var columns;
   $('[data-save]').addClass('hidden');
@@ -409,6 +449,21 @@ function saveCurrentData() {
     }
   } else {
     columns = trimColumns(table.getColumns());
+  }
+
+  var emptyColumns = getEmptyColumns(columns, entries);
+
+  _.forEach(emptyColumns, function(column) {
+    var columnIndex = columns.indexOf(column);
+
+    if (columnIndex !== -1) {
+      hot.alter('remove_col', columnIndex, 1, 'removeEmptyColumn');
+      columns.splice(columnIndex, 1);
+    }
+  });
+
+  if (entries.length && emptyColumns.length) {
+    entries = removeEmptyColumnsInEntries(entries, emptyColumns);
   }
 
   var widths = trimColumns(table.getColWidths());
