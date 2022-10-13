0542a5b72fbbae0fd28af42917992106372e7181
NJadhavXoriant <Nikita.Jadhav@Xoriant.Com>
2022-10-13 21:12:25 +0530

    Code refinement changes

--- js/interface.js
+++ js/interface.js
@@ -2382,18 +2382,10 @@ function getSecurityRule() {
   }
 
   if (currentFinalRules.length > 0) {
-    currentFinalRules.forEach(function(rule) {
-      if (!rule.allow.hasOwnProperty('tokens')) {
-        return;
-      }
-
-      if (widgetData.tokenId && Number(rule.allow.tokens[0]) !== selectedTokenId) {
-        if (Number(rule.allow.tokens[0]) === widgetData.tokenId) {
-          hasSecurityRule = true;
-        }
-      } else if (Number(rule.allow.tokens[0]) === selectedTokenId) {
-        hasSecurityRule = true;
-      }
+    hasSecurityRule = currentFinalRules.some(function(rule) {
+      return _.some(rule.allow.tokens, function(token) {
+        return token && (token === widgetData.tokenId || token === selectedTokenId);
+      });
     });
   }
 
