cb535e58c6b6b1529d1ce82370dc2493b19c282b
armine-fliplet <asimonyan@fliplet.com>
2025-11-25 13:51:18 +0400

    Revert "Merge branch 'master' into production"
    This reverts commit 3a49b7a1a8767ca35b37e1f09fc61f3522742718, reversing
    changes made to 84e9b4ca1a971ae439c5e599930bd468807711ac.

--- js/interface.js
+++ js/interface.js
@@ -81,11 +81,6 @@ var customRuleEditor = CodeMirror.fromTextArea($('#custom-rule')[0], {
   lineNumbers: true,
   mode: 'javascript'
 });
-var accessRulesEditor = CodeMirror.fromTextArea($('#access-rules-json')[0], {
-  lineNumbers: true,
-  mode: 'javascript'
-});
-
 
 var emptyColumnNameRegex = /^Column\s\([0-9]+\)$/;
 
@@ -266,38 +261,9 @@ function renderError(options) {
   });
 }
 
-function waitUntilSized(selector, callback) {
-  const el = document.querySelector(selector);
-
-  function check() {
-    if (!el || !document.contains(el)) {
-      return;
-    }
-
-    const rect = el.getBoundingClientRect();
-
-    if (rect.width > 0 && rect.height > 0) {
-      callback();
-    } else {
-      requestAnimationFrame(check);
-    }
-  }
-
-  check();
-}
-
-function renderSpreadsheet(rowsData) {
-  waitUntilSized('.table-entries', () => {
-    table = spreadsheet({ columns: columns, rows: rowsData });
-    $('.table-entries').css('visibility', 'visible').removeAttr('aria-busy');
-    $('#versions').removeClass('hidden');
-  });
-}
-
 function fetchCurrentDataSourceDetails() {
   definitionEditor.setValue('');
   hooksEditor.setValue('');
-  accessRulesEditor.setValue('[]');
 
   return Fliplet.DataSources.getById(currentDataSourceId, { cache: false }).then(function(dataSource) {
     $settings.find('#id').html(dataSource.id);
@@ -306,14 +272,9 @@ function fetchCurrentDataSourceDetails() {
     $('#bundle-online').prop('checked', dataSource.bundle === false);
 
     currentDataSourceType = dataSource.type;
-
-    let accessRules = Array.isArray(dataSource.accessRules) ? dataSource.accessRules : [];
-
-    currentDataSourceRules = Array.isArray(dataSource.accessRules) ? JSON.parse(JSON.stringify(dataSource.accessRules)) : dataSource.accessRules;
-    currentFinalRules = Array.isArray(dataSource.accessRules) ? JSON.parse(JSON.stringify(dataSource.accessRules)) : dataSource.accessRules;
+    currentDataSourceRules = dataSource.accessRules;
+    currentFinalRules = dataSource.accessRules;
     currentDataSourceDefinition = dataSource.definition || {};
-
-    accessRulesEditor.setValue(JSON.stringify(accessRules, null, 2));
 
     if (dataSource.apps && dataSource.apps.length > 0) {
       dataSourceIsLive = _.some(dataSource.apps, function(app) {
@@ -1568,7 +1529,6 @@ $('#app')
     var bundle = $('#bundle-online').is(':checked') ? false : true;
     var definition = definitionEditor.getValue();
     var hooks = hooksEditor.getValue();
-    var accessRulesValue = accessRulesEditor.getValue();
 
     $settings.find('#name').parents(':eq(1)').removeClass('has-error');
 
@@ -1603,39 +1563,6 @@ $('#app')
       return;
     }
 
-    var trimmedAccessRules = (accessRulesValue || '').trim();
-    var accessRulesData;
-
-    if (trimmedAccessRules) {
-      try {
-        var parsedAccessRules = JSON.parse(accessRulesValue);
-      } catch (e) {
-        Fliplet.Navigate.popup({
-          popupTitle: 'Invalid settings',
-          popupMessage: 'Access rules must be a valid JSON'
-        });
-
-        return;
-      }
-
-      if (Array.isArray(parsedAccessRules)) {
-        accessRulesData = parsedAccessRules;
-      } else if (parsedAccessRules && typeof parsedAccessRules === 'object' && Array.isArray(parsedAccessRules.accessRules)) {
-        accessRulesData = parsedAccessRules.accessRules;
-      } else {
-        Fliplet.Navigate.popup({
-          popupTitle: 'Invalid access rules',
-          popupMessage: 'Access rules JSON must be an array or an object containing an "accessRules" array.'
-        });
-
-        return;
-      }
-    } else {
-      accessRulesData = [];
-    }
-
-    var formattedAccessRules = JSON.stringify(accessRulesData, null, 2);
-
     try {
       hooks.forEach(function(hook) {
         if (typeof hook.type !== 'string' || !hook.type) {
@@ -1668,13 +1595,9 @@ $('#app')
       name: name,
       bundle: bundle,
       definition: definition,
-      hooks: hooks,
-      accessRules: accessRulesData
+      hooks: hooks
     })
       .then(function() {
-        currentDataSourceRules = JSON.parse(JSON.stringify(accessRulesData));
-        currentFinalRules = JSON.parse(JSON.stringify(accessRulesData));
-        accessRulesEditor.setValue(formattedAccessRules);
         // Update name on UI
         $('.editing-data-source-name').text(name);
 
@@ -1884,7 +1807,6 @@ $('#show-settings').click(function() {
   setTimeout(function() {
     definitionEditor.refresh();
     hooksEditor.refresh();
-    accessRulesEditor.refresh();
   }, 0);
 });
 
@@ -2474,8 +2396,8 @@ $('#show-access-rules').click(function() {
 
           return '-';
         })(),
-        apps: rule.appId
-          ? _.compact(rule.appId.map(function(appId) {
+        apps: rule.appId ?
+          _.compact(rule.appId.map(function(appId) {
             var app = _.find(apps, {
               id: appId
             });
@@ -2832,10 +2754,6 @@ function updateDataSourceRules() {
   return Fliplet.DataSources.update(currentDataSourceId, {
     accessRules: currentDataSourceRules
   }).then(function() {
-    var formattedRules = JSON.stringify(Array.isArray(currentDataSourceRules) ? currentDataSourceRules : [], null, 2);
-
-    currentFinalRules = JSON.parse(JSON.stringify(currentDataSourceRules));
-    accessRulesEditor.setValue(formattedRules);
     $saveButton.html(buttonLabel).removeClass('disabled').addClass('hidden');
 
     Fliplet.Modal.alert({
